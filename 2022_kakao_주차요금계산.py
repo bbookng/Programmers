1fafb2c14059cac924e5767c181fb156c4074ccc
bbookng <bbookng@gmail.com>
2022-12-01 17:31:33 +0900

    1201

--- 2022_kakao_주차요금계산.py
+++ 2022_kakao_주차요금계산.py
@@ -4,7 +4,7 @@ def solution(fees, records):
     answer = []
     parking = dict()
     total = dict()
-    
+
     for record in records:
         time, number, inout = record.split()
         if inout == 'IN':
@@ -36,6 +36,9 @@ def solution(fees, records):
         answer.append(fee)
 
     return answer
+
+
+print(solution([180, 5000, 10, 600], ["05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"]))
 
 '''
 테스트 1 〉	통과 (0.04ms, 10.5MB)
