6306aecf78131f7d3f7c88675e81b5754900c439
bbookng <bbookng@gmail.com>
2023-01-06 14:30:30 +0900

    0106

---
+++ kakao/2021_kakao_순위 검색.py
@@ -0,0 +1,10 @@
+def solution(info, query):
+    answer = []
+
+    for search in query:
+        lang, part, career, food = search.split('and')
+        food, score = food.split()
+
+    return answer
+
+print(solution(["java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"], ["java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"]))
