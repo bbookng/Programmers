48303e890db6e064b270e75402bc9d6b3634205d
bbookng <bbookng@gmail.com>
2023-01-11 08:01:27 +0900

    tmp

--- kakao/2019_kakao_후보키.py
+++ kakao/2019_kakao_후보키.py
@@ -1,7 +1,24 @@
 from itertools import combinations
 
 def solution(relation):
-    answer = 0
-    return answer
+    candidate_key = []
+    unique = []
+
+    for i in range(1, len(relation[0])):
+        candidate_key.extend(combinations([j for j in range(len(relation[0]))], i))
+
+    for candidate in candidate_key:
+        tmp = [set([relate[i] for i in candidate]) for relate in relation]
+        print(candidate)
+        print(tmp)
+
+        if len(tmp) == len(relation):
+
+
+
+
+
+
+    return candidate
 
 print(solution([["100","ryan","music","2"],["200","apeach","math","2"],["300","tube","computer","3"],["400","con","computer","4"],["500","muzi","music","3"],["600","apeach","music","2"]]))
