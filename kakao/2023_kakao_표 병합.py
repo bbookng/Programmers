85a1c71fb1459dfd46497bb72f5b5772b6c4a6ac
bbookng <bbookng@gmail.com>
2023-02-24 17:49:10 +0900

    웨않돼

--- kakao/2023_kakao_표 병합.py
+++ kakao/2023_kakao_표 병합.py
@@ -1,4 +1,15 @@
 from pprint import pprint
+
+
+def update(r, c, nr, nc, table):
+    if table[r][c][1] == [r, c]:
+        table[r][c] = [table[nr][nc][0], [table[nr][nc][1][0], table[nr][nc][1][1]]]
+    else:
+        for i in range(1, 51):
+            for j in range(1, 51):
+                if table[i][j][1] == table[r][c][1]:
+                    table[i][j] = [table[nr][nc][0], [table[nr][nc][1][0], table[nr][nc][1][1]]]
+
 def solution(commands):
     answer = []
     table = [[['', [r, c]] for c in range(51)] for r in range(51)]
@@ -18,12 +29,10 @@ def solution(commands):
                     if table[i][j][1] == [nr, nc]:
                         table[i][j][0] = value
 
-
-
         elif command == 'UPDATE' and len(value) == 2:
             value1, value2 = value[0], value[1]
-            for i in range(1, 51):
-                for j in range(1, 51):
+            for i in range(1, 10):
+                for j in range(1, 10):
                     if table[i][j][0] == value1:
                         table[i][j][0] = value2
 
@@ -32,17 +41,18 @@ def solution(commands):
             if r1 == r2 and c1 == c2:
                 continue
             if table[r1][c1][0] != '' and table[r2][c2][0] != '':
-                table[r2][c2] = [table[r1][c1][0], [table[r1][c1][1][0], table[r1][c1][1][1]]]
+                update(r2, c2, r1, c1, table)
             elif table[r1][c1][0] == '' and table[r2][c2][0] != '':
-                table[r1][c1] = [table[r2][c2][0], [table[r2][c2][1][0], table[r2][c2][1][1]]]
+                update(r1, c1, r2, c2, table)
             elif table[r1][c1][0] != '' and table[r2][c2][0] == '':
-                table[r2][c2] = [table[r1][c1][0], [table[r1][c1][1][0], table[r1][c1][1][1]]]
+                update(r2, c2, r1, c1, table)
             else:
                 table[r2][c2] = ['', [table[r1][c1][1][0], table[r1][c1][1][1]]]
 
         elif command == 'UNMERGE':
             r, c = int(value[0]), int(value[1])
             nr, nc = table[r][c][1]
+            tmp = []
             for i in range(1, 51):
                 for j in range(1, 51):
                     if i == r and j == c:
