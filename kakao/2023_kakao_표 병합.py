ab118b001fdbaa111ea78125417858a76997489e
bbookng <bbookng@gmail.com>
2023-03-01 21:58:39 +0900

    0301

--- kakao/2023_kakao_표 병합.py
+++ kakao/2023_kakao_표 병합.py
@@ -1,14 +1,12 @@
 from pprint import pprint
 
-
 def update(r, c, nr, nc, table):
-    if table[r][c][1] == [r, c]:
-        table[r][c] = [table[nr][nc][0], [table[nr][nc][1][0], table[nr][nc][1][1]]]
-    else:
-        for i in range(1, 51):
-            for j in range(1, 51):
-                if table[i][j][1] == table[r][c][1]:
-                    table[i][j] = [table[nr][nc][0], [table[nr][nc][1][0], table[nr][nc][1][1]]]
+    for i in range(1, 51):
+        for j in range(1, 51):
+            # 머지 당하는 셀과 부모가 같으면 모두 병합해줘야함
+            if table[i][j][1] == table[r][c][1]:
+                # 머지 하는 쪽 셀 내용으로 수정
+                table[i][j] = [table[nr][nc][0], table[nr][nc][1]]
 
 def solution(commands):
     answer = []
@@ -18,26 +16,27 @@ def solution(commands):
         command = command.split()
         command, value = command[0], command[1:]
 
+        # 셀 위치를 선택해 update 할 때
         if command == 'UPDATE' and len(value) == 3:
             r, c, value = int(value[0]), int(value[1]), value[2]
-            if r == table[r][c][1][0] and c == table[r][c][1][1]:
-                table[r][c][0] = value
-                continue
             nr, nc = table[r][c][1]
             for i in range(1, 51):
                 for j in range(1, 51):
                     if table[i][j][1] == [nr, nc]:
                         table[i][j][0] = value
 
+        # 셀 value 를 선택해 value2로 바꿀 때
         elif command == 'UPDATE' and len(value) == 2:
             value1, value2 = value[0], value[1]
-            for i in range(1, 10):
-                for j in range(1, 10):
+            for i in range(1, 51):
+                for j in range(1, 51):
                     if table[i][j][0] == value1:
                         table[i][j][0] = value2
 
+        # merge 할 때
         elif command == 'MERGE':
             r1, c1, r2, c2 = int(value[0]), int(value[1]), int(value[2]), int(value[3])
+            # 같은 위치면 무시
             if r1 == r2 and c1 == c2:
                 continue
             if table[r1][c1][0] != '' and table[r2][c2][0] != '':
@@ -47,7 +46,7 @@ def solution(commands):
             elif table[r1][c1][0] != '' and table[r2][c2][0] == '':
                 update(r2, c2, r1, c1, table)
             else:
-                table[r2][c2] = ['', [table[r1][c1][1][0], table[r1][c1][1][1]]]
+                update(r2, c2, r1, c1, table)
 
         elif command == 'UNMERGE':
             r, c = int(value[0]), int(value[1])
@@ -55,10 +54,16 @@ def solution(commands):
             tmp = []
             for i in range(1, 51):
                 for j in range(1, 51):
+                    # 선택한 셀일 경우는 값 그대로
                     if i == r and j == c:
                         table[i][j] = [table[i][j][0], [i, j]]
+                    # 아니면 수정할거니까 병합되어 있는 셀들의 좌표를 리스트에 추가
                     elif [nr, nc] == table[i][j][1]:
-                        table[i][j] = ['', [i, j]]
+                        tmp.append([i, j])
+
+            # 값 초기화
+            for i, j in tmp:
+                table[i][j] = ['', [i, j]]
 
         elif command == 'PRINT':
             r, c = int(value[0]), int(value[1])
