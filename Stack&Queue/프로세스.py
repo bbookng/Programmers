9e951e0c983f388e391402e7d02903b4e873e2bd
bokyung <bbookng@gmail.com>
2023-07-21 13:33:35 +0900

    [PGS] Lv2 / 프로세스

---
+++ Stack&Queue/프로세스.py
@@ -0,0 +1,14 @@
+import heapq
+
+def solution(priorities, location):
+    answer = 0
+    q = [(i, p) for i, p in enumerate(priorities)]
+
+    while True:
+        now = q.pop(0)
+        if any(now[1] < i[1] for i in q):
+            q.append(now)
+        else:
+            answer += 1
+            if now[0] == location:
+                return answer
