e15827f8965eb0f0646ac8a338d45c088d6f933e
bokyung <bbookng@gmail.com>
2023-08-03 15:06:52 +0900

    [PGS] Lv1 / 완주하지 못한 선수

--- Java_Practice/algorithm/src/완주하지_못한_선수.java
+++ Java_Practice/algorithm/src/완주하지_못한_선수.java
@@ -6,6 +6,7 @@ public class 완주하지_못한_선수 {
     public String solution(String[] participant, String[] completion) {
         Map<String, Integer> map = new HashMap<>();
         for (String p : participant) {
+            // getOrDefault :  찾는 키가 존재한다면 찾는 키의 값을 반환하고 없다면 기본 값을 반환하는 메서드
             map.put(p, map.getOrDefault(p, 0) + 1);
         }
 
@@ -13,6 +14,7 @@ public class 완주하지_못한_선수 {
             map.put(c, map.get(c) - 1);
         }
 
+        // hashmap의 key list 순회
         for (String key : map.keySet()) {
             if (map.get(key) != 0) {
                 answer = key;
