85c78b9c271461190ade1e0a257019532bc6f189
bbookng <bbookng@gmail.com>
2023-08-10 12:07:10 +0900

    [PGS] Lv1 / K번째 수

--- Java_Practice/algorithm/src/K번째수.java
+++ Java_Practice/algorithm/src/K번째수.java
@@ -11,4 +11,21 @@ public class K번째수 {
         }
         return answer;
     }
+
+    // 책 풀이
+    public int sol(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for (int i = 0; i < answer.length; i++) {
+            int[] command = commands[i];
+            int from = command[0] -1;
+            int to = command[1];
+            int k = command[2] -1;
+
+            int[] sub = Arrays.copyOfRange(array, from, to);
+            Arrays.sort(sub);
+            answer[i] = sub[k];
+        }
+        return answer;
+    }
 }
