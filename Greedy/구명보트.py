da6208567ca7ba25e44fcab2eb784b48478f690c
bbookng <bbookng@gmail.com>
2024-01-23 09:38:25 +0900

    [PGS] Lv2 / 구명보트

--- Greedy/구명보트.py
+++ Greedy/구명보트.py
@@ -1,3 +1,26 @@
 def solution(people, limit):
+    boats = [0]
+    for person in people:
+        for i in range(len(boats)):
+            if person + boats[i] <= limit:
+                boats[i] += person
+                break
+        else:
+            boats.append(person)
+
+    return len(boats)
+
+print(solution([70, 80, 50], 100))
+
+def solution(people, limit) :
     answer = 0
-    return answer
+    people.sort()
+
+    a = 0
+    b = len(people) - 1
+    while a < b :
+        if people[b] + people[a] <= limit :
+            a += 1
+            answer += 1
+        b -= 1
+    return len(people) - answer
