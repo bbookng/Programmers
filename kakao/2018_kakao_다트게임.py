c5ceb964620c0764a8c3e264b663f84ffa58f3f9
bbookng <bbookng@gmail.com>
2024-10-11 16:31:02 +0900

    [PGS] Lv1 / 다트게임

---
+++ kakao/2018_kakao_다트게임.py
@@ -0,0 +1,27 @@
+def solution(dartResult):
+    answer = 0
+    result = []
+
+    tmp_number = ''
+
+    for dart in dartResult:
+        if dart.isdigit():
+            tmp_number = tmp_number + dart
+        else:
+            if tmp_number != '':
+                result.append(int(tmp_number))
+                tmp_number = ''
+
+        if dart.isalpha():
+            if dart == 'D':
+                result[-1] = result[-1] ** 2
+            elif dart == 'T':
+                result[-1] = result[-1] ** 3
+        elif dart == '*':
+            result[-1] *= 2
+            if len(result) != 1:
+                result[-2] *= 2
+        elif dart == '#':
+            result[-1] *= -1
+
+    return sum(result)
