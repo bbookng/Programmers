1cad1defbc39a93e1267a155a10805384839da5e
bbookng <bbookng@gmail.com>
2024-11-04 16:57:39 +0900

    [PGS] Lv1 / 추억 점수

--- kakao/2020_kakao_문자열 압축.py
+++ kakao/2020_kakao_문자열 압축.py
@@ -54,3 +54,30 @@ def solution(s):
 테스트 27 〉	통과 (3.02ms, 10.3MB)
 테스트 28 〉	통과 (0.02ms, 10.3MB)
 '''
+
+def solution(s):
+    answer = []
+
+    if len(s) == 1:
+        return 1
+
+    for i in range(1, len(s)//2 + 1):
+        result = ''
+        tmp = s[:i]
+        cnt = 1
+        for j in range(i, len(s), i):
+            if tmp == s[j:j+i]:
+                cnt += 1
+            else:
+                if cnt > 1:
+                    result += str(cnt) + tmp
+                else:
+                    result += tmp
+                cnt = 1
+                tmp = s[j:j+i]
+        if cnt > 1:
+            result += str(cnt) + tmp
+        else:
+            result += tmp
+        answer.append(len(result))
+    return min(answer)
